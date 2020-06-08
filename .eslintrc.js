8995004919aea442b68b79f4ad25809ee2631f6a
liangyuanruo <liangyuanruo@gmail.com>
2020-06-08 17:40:58 +0800

    feat(lint): configure typescript member delimiter style

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,19 @@ module.exports = {
   rules: {
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'no-console': 'warn',
+    '@typescript-eslint/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'semi',
+          requireLast: false,
+        },
+      },
+    ],
   },
   ignorePatterns: ['build'],
 }
