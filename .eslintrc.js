fb4f564c8e18980543c26491034fa221506ed26b
liangyuanruo <liangyuanruo@gmail.com>
2020-06-08 17:21:15 +0800

    feat(lint): incorporate @typescript-eslint/recommended

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
