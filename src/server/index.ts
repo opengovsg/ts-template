bfac200a802284436949ca1a3b8f5d4ff8f56045
LoneRifle <LoneRifle@users.noreply.github.com>
2020-06-09 14:35:50 +0800

    fix(server): only log the port, don't assume localhost

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,5 +6,5 @@ const port = Number(process.env.PORT)
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Listening on port ${port}`)
 )
