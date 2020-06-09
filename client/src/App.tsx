a3cd06664c2c16e73687bac600ee96c8d1fa5329
liangyuanruo <liangyuanruo@gmail.com>
2020-06-09 21:50:30 +0800

    feat(client): finish client build and verify setup with test fetch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [greeting, setGreeting] = useState('')
+
+  useEffect(() => {
+    fetch('/api/hello').then(response => {
+      return response.text()
+    }).then(data => setGreeting(data))
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{ greeting }</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,8 @@ import express from 'express'
 const app = express()
 const port = Number(process.env.PORT) || 8080
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api/hello', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('build/client'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
