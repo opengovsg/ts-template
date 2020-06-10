2c6ea0f90c2e835c6deaad8d5ab67f7da9a20703
liangyuanruo <liangyuanruo@gmail.com>
2020-06-10 10:34:20 +0800

    refactor(server): reorder middleware to serve static files first

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,7 @@ import express from 'express'
 const app = express()
 const port = Number(process.env.PORT) || 8080
 
-app.get('/api/hello', (req, res) => res.send('Hello World!'))
-
 app.use(express.static('build/client'))
+app.get('/api/hello', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
