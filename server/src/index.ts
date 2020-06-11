978ac82e8671dd9c8b8b524eabd785c142d17c3d
LoneRifle <LoneRifle@users.noreply.github.com>
2020-06-11 10:31:43 +0800

    refactor: move server deps, rework npm config
    - Replace run scripts duplicated for client and server with new ones,
      `on-server` and `on-client`, that can take arguments through npm,
      supplied after a `--` separator
    - Isolate server dependencies to within `server/` and run the build in
      that dir
    - Move lint-staged to server, get husky at root to invoke lint-staged
      on client (stubbed) and server
    
    TODO: eslint and lint-staged for client

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 const port = Number(process.env.PORT) || 8080
 
-app.use(express.static('build/client'))
-app.get('/api/hello', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.resolve(__dirname + '/../../client/build')))
+app.get('/api/hello', (_req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
