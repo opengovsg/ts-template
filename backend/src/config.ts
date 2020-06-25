890a61fe6aba87c57e05c6873d007e363b1a73d7
LoneRifle <LoneRifle@users.noreply.github.com>
2020-06-25 15:20:32 +0800

    feat(config): configure backend using convict
    - `npm install convict @types/convict`
    - parse env var `PORT` via convict, injecting the value as the port
      that express will listen on

---
+++ backend/src/config.ts
@@ -0,0 +1,39 @@
+/**
+ * @file Configuration
+ * All defaults can be changed
+ */
+import convict from 'convict'
+
+/**
+ * To require an env var without setting a default,
+ * use
+ *    default: '',
+ *    format: 'required-string',
+ *    sensitive: true,
+ */
+convict.addFormats({
+  'required-string': {
+    validate: (val: unknown): void => {
+      if (val === '') {
+        throw new Error('Required value cannot be empty')
+      }
+    },
+    coerce: <T extends unknown>(val: T): T | undefined => {
+      if (val === null) {
+        return undefined
+      }
+      return val
+    },
+  },
+})
+
+const config = convict({
+  port: {
+    doc: 'The port that the service listens on',
+    env: 'PORT',
+    format: 'int',
+    default: 8080,
+  },
+})
+
+export default config
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import path from 'path'
 
+import config from './config'
+
 const app = express()
-const port = Number(process.env.PORT) || 8080
+const port = config.get('port')
 
 app.use(express.static(path.resolve(__dirname + '/../../client/build')))
 app.get('/api/hello', (_req, res) => res.send('Hello World'))
