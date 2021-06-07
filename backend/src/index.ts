040b28c896bd3d4cc56f6fb4f63061c1abdc0b5d
LoneRifle <alwyn.tan@gmail.com>
2021-06-07 05:53:48 +0000

    chore: fix typos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import config from './config'
 const app = express()
 const port = config.get('port')
 
-app.use(express.static(path.resolve(__dirname + '/../../client/build')))
+app.use(express.static(path.resolve(__dirname + '/../../frontend/build')))
 app.get('/api/hello', (_req, res) => res.send('Hello World'))
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
