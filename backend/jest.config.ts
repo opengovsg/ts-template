8004cb52834aa4aa6017e9d644b4a68ed3eb0e52
Lam Kee Wei <keewei.lam@gmail.com>
2021-08-13 14:52:17 +0800

    test: add jest configuration

---
+++ backend/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/node_modules/'
+  ],
+}
+
+export default config
