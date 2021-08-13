342f9bb9a365aa7a472f1b9f7b8e39ea2140d7ad
Lam Kee Wei <keewei.lam@gmail.com>
2021-08-13 17:15:28 +0800

    chore: add command to calculate coverage

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -3,9 +3,13 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: [
-    '<rootDir>/build/',
-    '<rootDir>/node_modules/'
+  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,js}'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/build',
+    '<rootDir>/node_modules',
+    '<rootDir>/src/tests',
+    '<rootDir>/src/database/migrations',
   ],
 }
 
