df7d72416efb4759640429dfd1eff1805b509fde
Kar Rui Lau <karrui.lau@gmail.com>
2021-08-13 16:38:01 +0800

    feat(client): add LoginPage component

---
+++ frontend/src/pages/login/LoginPage.tsx
@@ -0,0 +1,199 @@
+import { FC, useMemo, useState } from 'react'
+import {
+  Box,
+  Link,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  useBreakpointValue,
+  Wrap,
+  chakra,
+} from '@chakra-ui/react'
+
+import { LoginForm, LoginFormInputs } from './components/LoginForm'
+import { OtpForm, OtpFormInputs } from './components/OtpForm'
+import { useAuth } from '../../features/auth/AuthContext'
+
+import { ReactComponent as BrandLogoSvg } from '../../assets/svgs/brand-hort-colour.svg'
+import { ReactComponent as LoginImageSvg } from '../../assets/svgs/login.svg'
+
+const LoginImage = chakra(LoginImageSvg)
+const BrandLogo = chakra(BrandLogoSvg)
+
+export type LoginOtpData = {
+  email: string
+}
+
+export const LoginPage: FC = () => {
+  const [email, setEmail] = useState<string>()
+  const { sendLoginOtp, verifyLoginOtp } = useAuth()
+
+  const currentYear = new Date().getFullYear()
+  const isDesktop = useBreakpointValue({ base: false, xs: false, lg: true })
+  const isTablet = useBreakpointValue({ base: false, xs: false, md: true })
+
+  const footerLinks = useMemo(
+    () => [
+      { label: 'Contact Us', href: '' },
+      { label: 'Guide', href: '' },
+      { label: 'Privacy', href: '' },
+      { label: 'Terms of Use', href: '' },
+      { label: 'Report Vulnerability', href: '' },
+    ],
+    [],
+  )
+
+  const handleSendOtp = async ({ email }: LoginFormInputs) => {
+    await sendLoginOtp(email)
+    return setEmail(email)
+  }
+
+  const handleVerifyOtp = ({ otp }: OtpFormInputs) => {
+    // Should not happen, since OtpForm component is only shown when there is
+    // already an email state set.
+    if (!email) {
+      throw new Error('Something went wrong')
+    }
+    return verifyLoginOtp({ token: otp, email })
+  }
+
+  const handleResendOtp = async () => {
+    // Should not happen, since OtpForm component is only shown when there is
+    // already an email state set.
+    if (!email) {
+      throw new Error('Something went wrong')
+    }
+    await sendLoginOtp(email)
+  }
+
+  return (
+    <Flex flexDir="column" minH="100vh">
+      <Box
+        flexGrow={1}
+        px={{ base: '1.5rem', md: '5.5rem', lg: 0 }}
+        bg={{
+          base: 'initial',
+          md: 'linear-gradient(180deg, var(--chakra-colors-primary-500) 20.625rem, white 0)',
+          lg: 'linear-gradient(90deg, var(--chakra-colors-primary-500) 42%, white 0)',
+        }}
+      >
+        <Grid
+          minH={{ base: 'initial', lg: '100vh' }}
+          maxW="90rem"
+          margin="auto"
+          templateAreas={{
+            base: `'login'`,
+            md: `'sidebar' 'login'`,
+            lg: `'sidebar login' 'copy links'`,
+          }}
+          templateRows={{ lg: '1fr auto' }}
+          templateColumns={{ lg: '5fr 7fr' }}
+        >
+          {isTablet && (
+            <GridItem
+              d="flex"
+              gridArea="sidebar"
+              bg={{ base: 'transparent', lg: 'primary.500' }}
+              px={{ base: '1.5rem', lg: '5rem' }}
+              pt={{ base: '1.5rem', md: '4rem', lg: '6rem' }}
+              pb={{ lg: '6rem' }}
+              flexDir="column"
+              alignItems="flex-start"
+              justifyContent="center"
+            >
+              <BrandLogo
+                title="Makeshift template logo"
+                mb="1.5rem"
+                h="2rem"
+                display={{ base: 'none', lg: 'initial' }}
+              />
+              <Text
+                display={{ base: 'none', lg: 'initial' }}
+                textStyle="display-2"
+                color="white"
+                mb="2.5rem"
+              >
+                Fuss-free project templates
+              </Text>
+              <LoginImage
+                aria-hidden
+                maxH={{ base: '22rem', lg: '28rem' }}
+                w="100%"
+              />
+            </GridItem>
+          )}
+
+          <GridItem
+            h="100%"
+            gridArea="login"
+            px={{ base: 0, lg: '7.25rem' }}
+            py="4rem"
+            d="flex"
+            alignItems={{ base: 'initial', lg: 'center' }}
+          >
+            <Box
+              maxW={{ base: '100%', lg: '28rem' }}
+              w="100%"
+              minH={{ base: 'auto', lg: '24rem' }}
+            >
+              <Flex flexDir="column" align="flex-start">
+                <BrandLogo
+                  d={{ base: 'initial', lg: 'none' }}
+                  title="Makeshift template logo"
+                  mb="1.5rem"
+                  h={{ base: '1.5rem', lg: '2rem' }}
+                />
+                <Text
+                  textStyle="h3"
+                  color="secondary.500"
+                  mb={{ base: '2rem', lg: '2.5rem' }}
+                >
+                  Get started with Template
+                </Text>
+              </Flex>
+              {!email ? (
+                <LoginForm onSubmit={handleSendOtp} />
+              ) : (
+                <OtpForm
+                  onSubmit={handleVerifyOtp}
+                  onResendOtp={handleResendOtp}
+                />
+              )}
+            </Box>
+          </GridItem>
+          {isDesktop && (
+            <>
+              <GridItem
+                gridArea="copy"
+                bg={{ base: 'transparent', lg: 'primary.500' }}
+                px={{ base: '1.5rem', lg: '5rem' }}
+                pt="0.5rem"
+                pb="4rem"
+              >
+                <Text textStyle="legal" color="white">
+                  © {currentYear} Open Government Products, GovTech Singapore
+                </Text>
+              </GridItem>
+              <GridItem
+                px={{ base: '1.5rem', lg: '7.25rem' }}
+                pt="0.5rem"
+                pb="4rem"
+                display={{ base: 'none', lg: 'flex' }}
+                gridArea="links"
+              >
+                <Wrap shouldWrapChildren textStyle="legal" spacing="1.5rem">
+                  {footerLinks.map(({ label, href }, index) => (
+                    <Link key={index} href={href}>
+                      {label}
+                    </Link>
+                  ))}
+                </Wrap>
+              </GridItem>
+            </>
+          )}
+        </Grid>
+      </Box>
+    </Flex>
+  )
+}
---
+++ frontend/src/pages/login/components/LoginForm.tsx
@@ -0,0 +1,88 @@
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import {
+  FormControl,
+  Stack,
+  useBreakpointValue,
+  Button,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  Link,
+  Text,
+} from '@chakra-ui/react'
+import isEmail from 'validator/lib/isEmail'
+
+export type LoginFormInputs = {
+  email: string
+}
+
+interface LoginFormProps {
+  onSubmit: (inputs: LoginFormInputs) => Promise<void>
+}
+
+export const LoginForm = ({ onSubmit }: LoginFormProps): JSX.Element => {
+  const { handleSubmit, register, formState, setError } =
+    useForm<LoginFormInputs>()
+
+  const validateEmail = useCallback((value: string) => {
+    const isValidEmail = isEmail(value)
+    if (!isValidEmail) {
+      return 'Please enter a valid email'
+    }
+
+    const isGovDomain = value.split('@').pop()?.includes('gov.sg')
+    return isGovDomain || 'Please sign in with a gov.sg email address.'
+  }, [])
+
+  const onSubmitForm = async (inputs: LoginFormInputs) => {
+    return onSubmit(inputs).catch((e) => {
+      setError('email', { type: 'server', message: e.message })
+    })
+  }
+
+  const isMobile = useBreakpointValue({ base: true, xs: true, lg: false })
+
+  return (
+    <form onSubmit={handleSubmit(onSubmitForm)}>
+      <FormControl
+        isInvalid={!!formState.errors.email}
+        isReadOnly={formState.isSubmitting}
+        mb="2.5rem"
+      >
+        <FormLabel htmlFor="email" fontSize={{ base: '1.125rem', lg: '1rem' }}>
+          <Text mb="0.25rem">Email</Text>
+          <Text textStyle="body-2" mb="0.75rem">
+            Only available for use by public officers with a gov.sg email
+          </Text>
+        </FormLabel>
+        <Input
+          autoComplete="email"
+          autoFocus
+          placeholder="e.g. jane@data.gov.sg"
+          {...register('email', {
+            required: 'Please enter an email address',
+            validate: validateEmail,
+          })}
+        />
+        {formState.errors.email && (
+          <FormErrorMessage>{formState.errors.email.message}</FormErrorMessage>
+        )}
+      </FormControl>
+      <Stack
+        direction={{ base: 'column', lg: 'row' }}
+        spacing={{ base: '1.5rem', lg: '2.5rem' }}
+        align="center"
+      >
+        <Button
+          isFullWidth={isMobile}
+          isLoading={formState.isSubmitting}
+          type="submit"
+        >
+          Sign in
+        </Button>
+        <Link>Have a question?</Link>
+      </Stack>
+    </form>
+  )
+}
