b74c6a79d3f971d4619792e8c39d1db737c6a02a
Lam Kee Wei <keewei.lam@gmail.com>
2021-08-18 00:56:47 +0800

    feat: set app global configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+
 import { AppModule } from './app.module'
+import { ConfigService } from 'config/config.service'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+
   app.setGlobalPrefix('/api')
-  await app.listen(8080)
+
+  const config = app.get(ConfigService)
+  const environment = config.get('environment')
+  if (['staging', 'production'].includes(environment)) {
+    app.set('trust proxy', 1)
+  }
+
+  await app.listen(config.get('port'))
 }
 
 bootstrap()
