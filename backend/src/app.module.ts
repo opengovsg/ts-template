94765b0156f78e86101445d5c8bfb2011c3f779c
Preston Lim <prestonlimlianjie@gmail.com>
2021-08-27 13:09:29 +0800

    feat: import SequelizeModule and remove unused app controller/service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common'
-
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-
+import { SequelizeModule } from '@nestjs/sequelize'
 import { HelmetMiddleware } from 'middlewares/helmet.middleware'
 import { SessionMiddleware } from 'middlewares/session.middleware'
 import { ConfigModule } from 'config/config.module'
 
 @Module({
-  controllers: [AppController],
-  providers: [AppService],
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite', // TO-DO: change to production database dialect
+      autoLoadModels: true, // TO-DO: remove in production
+      synchronize: true, // TO-DO: remove in production
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
