f9046e8205e09df54b5beaff666b6e55e47291be
Preston Lim <prestonlimlianjie@gmail.com>
2021-08-27 14:13:57 +0800

    feat: inject ConfigModule into AuthModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { SequelizeModule } from '@nestjs/sequelize'
 import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
+import { ConfigModule } from '../config/config.module'
 import { User } from '../database/models'
 
 @Module({
-  imports: [SequelizeModule.forFeature([User])],
+  imports: [ConfigModule, SequelizeModule.forFeature([User])],
   controllers: [AuthController],
   providers: [AuthService],
 })
