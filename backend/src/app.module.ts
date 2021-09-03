0527e1d286c3ef517a4c40777985b2010994531b
Preston Lim <prestonlimlianjie@gmail.com>
2021-09-03 17:32:28 +0800

    feat: inject otp and mailer modules into app

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,14 @@ import { HelmetMiddleware } from 'middlewares/helmet.middleware'
 import { SessionMiddleware } from 'middlewares/session.middleware'
 import { ConfigModule } from 'config/config.module'
 import { AuthModule } from 'auth/auth.module'
+import { OtpModule } from 'otp/otp.module'
+import { MailerModule } from 'mailer/mailer.module'
 
 @Module({
   imports: [
     ConfigModule,
+    OtpModule,
+    MailerModule,
     SequelizeModule.forRoot({
       dialect: 'sqlite', // TO-DO: change to production database dialect
       autoLoadModels: true, // TO-DO: remove in production
