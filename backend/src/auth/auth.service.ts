3f840d82565cf22f390f38f01d1eb0344690712f
Preston Lim <prestonlimlianjie@gmail.com>
2021-09-03 17:31:15 +0800

    feat: inject otp and mailer modules into auth module

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -3,44 +3,40 @@ import { InjectModel } from '@nestjs/sequelize'
 import { GenerateOtpDto, VerifyOtpDto } from './dto/index'
 import { User } from '../database/models'
 import { Logger } from '@nestjs/common'
-import { totp as totpFactory } from 'otplib'
-import { ConfigService } from '../config/config.service'
+import { OtpService } from '../otp/otp.service'
+import { MailerService } from '../mailer/mailer.service'
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectModel(User)
     private readonly userModel: typeof User,
-    private config: ConfigService
+    private otpService: OtpService,
+    private mailerService: MailerService
   ) {}
 
-  private totp = totpFactory.clone({
-    step: this.config.get('otp.expiry'),
-    window: [1, 0],
-  })
-
-  private generateSecret(email: string) {
-    return this.config.get('otp.secret') + email
-  }
-
   async generateOtp(generateOtpDto: GenerateOtpDto): Promise<void> {
-    const otp = this.totp.generate(this.generateSecret(generateOtpDto.email))
-    const timeLeft = this.totp.options.step
-      ? Math.floor(this.totp.options.step / 60) // Round down to minutes
-      : NaN
+    const { email } = generateOtpDto
+    const { token, timeLeft } = this.otpService.generateOtp(email)
 
-    // TO-DO: Make this log only in development - read env from config
-    Logger.log(
-      `Your OTP token is ${otp}. It will expire in ${timeLeft} minutes`
-    )
+    const html = `Your OTP is <b>${token}</b>. It will expire in ${timeLeft} minutes.
+    Please use this to login to your account.
+    <p>If your OTP does not work, please request for a new one.</p>`
+
+    const mail = {
+      to: email,
+      from: 'Starter Kit <donotreply@mail.open.gov.sg>',
+      subject: 'One-Time Password (OTP) for Starter Kit',
+      html,
+    }
+
+    Logger.log(`Sending mail to ${email}`)
+    return this.mailerService.sendMail(mail)
   }
 
   async verifyOtp(verifyOtpDto: VerifyOtpDto): Promise<User | undefined> {
     const { email, token } = verifyOtpDto
-    const isVerified = this.totp.verify({
-      secret: this.generateSecret(email),
-      token,
-    })
+    const isVerified = this.otpService.verifyOtp(email, token)
     const [user] = isVerified
       ? await this.userModel.findOrCreate({ where: { email } })
       : []
