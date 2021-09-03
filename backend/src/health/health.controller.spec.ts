4ce5daa00519a03e810bd30dbb48fb42024858a6
Jason Chong <jason@Jasons-MacBook-Pro.local>
2021-09-03 17:11:46 +0800

    fix: fixes wrong import path in health module test

--- backend/src/health/health.controller.spec.ts
+++ backend/src/health/health.controller.spec.ts
@@ -1,6 +1,6 @@
 import { TerminusModule } from '@nestjs/terminus'
 import { Test, TestingModule } from '@nestjs/testing'
-import { ConfigModule } from 'config/config.module'
+import { ConfigModule } from '../config/config.module'
 import { HealthController } from './health.controller'
 
 describe('HealthController', () => {
