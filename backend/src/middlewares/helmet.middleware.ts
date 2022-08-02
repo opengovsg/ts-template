a68bcf00f3f08348e59883fb615b1d1ce4f02010
Niam Jen Wei <njenwei@gmail.com>
2022-08-02 17:27:29 +0800

    feat: add basic helmet config

--- backend/src/middlewares/helmet.middleware.ts
+++ backend/src/middlewares/helmet.middleware.ts
@@ -7,7 +7,32 @@ export class HelmetMiddleware implements NestMiddleware {
   private middleware: RequestHandler
 
   constructor() {
-    this.middleware = helmet()
+    this.middleware = helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          baseUri: ["'self'"],
+          blockAllMixedContent: [],
+          connectSrc: ["'self'"],
+          workerSrc: [],
+          // for google fonts
+          fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+          frameSrc: [],
+          frameAncestors: ["'none'"],
+          imgSrc: ["'self'", 'data:'],
+          objectSrc: ["'none'"],
+          // for google fonts
+          styleSrc: [
+            "'self'",
+            "'unsafe-inline'",
+            'https://fonts.googleapis.com',
+          ],
+          scriptSrcAttr: ["'none'"],
+          scriptSrc: ["'self'"],
+          upgradeInsecureRequests: null, // set to [] in production
+        },
+      },
+    })
   }
 
   use(req: Request, res: Response, next: NextFunction): void {
