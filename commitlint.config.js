c3987b2e2ba58a08a4d829e77632cc17e3d4d64d
Khant <9334746+the-robot@users.noreply.github.com>
2022-09-28 11:33:22 +0800

    fix: config commitlint to ignore dependabot commits (#1707)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => /^Bumps \[(.+)\]\((.+)\)(.*).$/m.test(message)],
   rules: {
     'scope-case': [2, 'always', ['pascal-case', 'lower-case', 'camel-case']],
   },
