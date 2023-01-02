2b9bb601c73bb056a686a6c1905bf4e03b7a8d47
Kar Rui <karrui.lau@gmail.com>
2023-01-03 06:10:30 +0800

    feat: remove unused ~assets path alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,7 +64,6 @@ module.exports = {
           ['^(~shared)(/.*|$)'],
           ['^(~)(/.*|$)'],
           [
-            '^(~assets)(/.*|$)',
             '^(~contexts)(/.*|$)',
             '^(~constants)(/.*|$)',
             '^(~hooks)(/.*|$)',
