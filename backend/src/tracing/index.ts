3ca371a55046c58fff51726a16e212c0e11ada6b
LoneRifle <LoneRifle@users.noreply.github.com>
2023-03-09 13:36:56 +0800

    build(lint): fix warnings relating to named imports

--- backend/src/tracing/index.ts
+++ backend/src/tracing/index.ts
@@ -1,8 +1,8 @@
 // https://docs.datadoghq.com/tracing/trace_collection/dd_libraries/nodejs/
 
-import tracer from 'dd-trace'
+import ddTrace from 'dd-trace'
 
-tracer.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
+ddTrace.init({ logInjection: true }) // initialized in a different file to avoid hoisting.
 
 export { TraceIdProvider } from './trace-id.provider'
-export default tracer
+export default ddTrace
